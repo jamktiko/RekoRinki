7a295dd58eb09b2cd2bfd4ae25e55c96e6ff19fe
landezi <151929467+landezi@users.noreply.github.com>
2025-10-02 12:14:43 +0300

    poistoja

--- frontend/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<h1>Hello World from Angular!</h1>`,
-  styles: []
-})
-export class AppComponent {}
