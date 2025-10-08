c94cd049307a3cacb8d682f03f52cf1dc8866e5a
landezi <151929467+landezi@users.noreply.github.com>
2025-10-08 12:02:57 +0300

    Delete testiymparisto directory

--- testiymparisto/src/Dekki.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { createEventDispatcher } from 'svelte';
-
-  const dispatch = createEventDispatcher();
-
-  export function lisaaDekkiin(kortti) {
-    dispatch(kortti);
-  }
-</script>
