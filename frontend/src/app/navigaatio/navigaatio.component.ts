ff25106f52866047e82bde953983e93284acd7e2
Maya Ramo <ad5721@student.jamk.fi>
2025-10-30 12:45:13 +0200

    tuotemäärä korjaaminen navigaatio komponenttissa

--- frontend/src/app/navigaatio/navigaatio.component.ts
+++ frontend/src/app/navigaatio/navigaatio.component.ts
@@ -14,4 +14,23 @@ export class NavigaatioComponent {
   readonly cstore = inject(CartStore);
 
   isMenuOpen = false;
+  showCartAlert = false; // tuotemäärä näkyminen menu kuvakeella
+
+  // Kun cstore.totalCount() kasvaa (eli tuote lisätään ostoskoriin),
+  // ngDoCheck() huomaa sen ja asettaa showCartAlert = true
+  ngDoCheck() {
+    // jos ostoskoriin lisätään jotain
+    if (this.cstore.totalCount() > 0 && !this.isMenuOpen) {
+      this.showCartAlert = true;
+    }
+  }
+
+  // kun panetaan toggleMenu kuvake piilotetaan tuotemäärä kuvaken päälle.
+  // taas kun suljetaan se menu kuvake näkyy tuotemäärä sinne menu kuvakken päälle
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.showCartAlert = false; // piilotetaan ilmoitus kun menu avataan
+    }
+  }
 }
