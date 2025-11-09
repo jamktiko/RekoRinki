8accf9e5a8fb7219f110b026d2d5cc93d83a32ca
Maya Ramo <ad5721@student.jamk.fi>
2025-11-09 20:29:15 +0200

    validointi korjaaminen kirjadu sisään sivussa

--- frontend/src/app/profiili/profiili.component.ts
+++ frontend/src/app/profiili/profiili.component.ts
@@ -11,8 +11,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { Router } from 'express';
+// import { Router } from 'express';
 import { CommonModule } from '@angular/common';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-profiili',
@@ -39,24 +40,18 @@ export class ProfiiliComponent {
   // luodaan lomake turvallisesti fb:n avulla
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+    password: [
+      '',
+      [
+        Validators.required, // kenttä ei saa olla tyhjä
+        Validators.minLength(6), // vähintään 6 merkkiä
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[!@#$%^&*?]).{6,}$/), // salasana sisältää vähintään yhden ison kirjaimen ja yhden erikoismerkin
+      ],
+    ],
   });
 
   hide = true; // salasanan näyttö/ piilotus
   loading = false; // lataustila napille
-
-  // onSubmit(): void {
-  //   if (this.loginForm.valid) {
-  //     const { email, password } = this.loginForm.value;
-  //     console.log('Kirjautumistiedot:', email, password);
-  //     // Tyhjennetään kentät onnistuneen kirjautumisen jälkeen
-  //     this.loginForm.reset();
-  //     // tänne voit myöhemmin lisätä API-kutsun tai reitityksen
-  //   } else {
-  //     console.log('Lomake ei ole kelvollinen');
-  //     this.loginForm.markAllAsTouched();
-  //   }
-  // }
 
   // Lähetystapahtuma
   onSubmit(): void {
@@ -70,10 +65,13 @@ export class ProfiiliComponent {
         this.loading = false;
         alert('Kirjautuminen onnistui!');
 
-        // Tyhjennä lomake ilman virheväriä
-        this.loginForm.reset();
-        // this.loginForm.markAsPristine();
-        // this.loginForm.markAsUntouched();
+        this.loginForm.reset(); // tyhjentää arvot
+        Object.keys(this.loginForm.controls).forEach((key) => {
+          const control = this.loginForm.get(key);
+          control?.setErrors(null); // poistaa mahdolliset virheet
+          control?.markAsPristine(); // kertoo, ettei käyttäjä ole vielä muokannut mitään
+          control?.markAsUntouched(); // kertoo, ettei käyttäjä ole vielä “käynyt” kentissä
+        });
       }, 1500);
     } else {
       console.log('Lomake ei ole kelvollinen');
