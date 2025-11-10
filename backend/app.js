01c0e080d9ded48f9ceb1509574f8171bfc7c718
korhonenjesse <ad2262@student.jamk.fi>
2025-11-10 16:39:09 +0200

    tehty ilmoitusten hakumetodi, sekä korjattu modeli tietokannan mukaiseksi

--- backend/app.js
+++ backend/app.js
@@ -21,5 +21,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 export default app;
