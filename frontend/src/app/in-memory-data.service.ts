7d495bec61805be423ef8775788dd5f5a096b760
Maya Ramo <ad5721@student.jamk.fi>
2025-11-16 23:02:17 +0200

    TuottajaID korjaaminen

--- frontend/src/app/in-memory-data.service.ts
+++ frontend/src/app/in-memory-data.service.ts
@@ -10,9 +10,11 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     // products-taulukko on valetietokanta, jossa ovat tuotteet
     const products: Product[] = [
+      // Nisulan tila (producerID: 1)
       {
         id: 1,
-        // producerID: 1,
+        notificationID: 1,
+        producerID: 1,
         name: 'Mansikka',
         price: 8,
         amount: 500,
@@ -21,10 +23,12 @@ export class InMemoryDataService implements InMemoryDbService {
         description:
           'Tuoretta, täysmehuaista mansikkaa, joka on poimittu aamutuimaan. Tuoksuu kesältä, tiiviitä ja mehuaista.',
         unit: 'g',
+        uniqueId: `${1}_${1}`,
       },
       {
         id: 2,
-        // producerID: 2,
+        notificationID: 1,
+        producerID: 1,
         name: 'Vadelma',
         price: 6,
         amount: 500,
@@ -32,10 +36,12 @@ export class InMemoryDataService implements InMemoryDbService {
         totalprice: 2000,
         description: 'Täysikypsää, tummaa ja raikkaita marjoja.',
         unit: 'g',
+        uniqueId: `${2}_${1}`,
       },
       {
         id: 3,
-        // producerID: 3,
+        notificationID: 1,
+        producerID: 1,
         name: 'Pensasmustikkka',
         price: 10,
         amount: 500,
@@ -44,21 +50,39 @@ export class InMemoryDataService implements InMemoryDbService {
         description:
           'Aitoa suomalaista pensasmustikkaa, täynnä antioksydantteja.',
         unit: 'g',
+        uniqueId: `${3}_${1}`,
       },
+      // Apilakosken tila (producerID: 3)
       {
         id: 4,
-        // producerID: 4,
-        name: 'Peruna',
-        price: 3,
+        notificationID: 3,
+        producerID: 3,
+        name: 'Mansikka',
+        price: 8,
         amount: 500,
         inventory: 100,
         totalprice: 1500,
         description: 'Tuoreita perunoita suoraan tilalta.',
         unit: 'g',
+        uniqueId: `${4}_${3}`,
       },
       {
         id: 5,
-        // producerID: 5,
+        notificationID: 3,
+        producerID: 3,
+        name: 'Peruna',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita.',
+        unit: 'g',
+        uniqueId: `${5}_${3}`,
+      },
+      {
+        id: 6,
+        notificationID: 3,
+        producerID: 3,
         name: 'Porkkana',
         price: 3,
         amount: 500,
@@ -66,6 +90,171 @@ export class InMemoryDataService implements InMemoryDbService {
         totalprice: 1200,
         description: 'Makeita ja rapeita porkkanoita.',
         unit: 'g',
+        uniqueId: `${6}_${3}`,
+      },
+      // Pekan tila (producerID: 5)
+      {
+        id: 7,
+        notificationID: 5,
+        producerID: 5,
+        name: 'Mansikka',
+        price: 8,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita.',
+        unit: 'g',
+        uniqueId: `${7}_${5}`,
+      },
+      {
+        id: 8,
+        notificationID: 5,
+        producerID: 5,
+        name: 'Porkkana',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita.',
+        unit: 'g',
+        uniqueId: `${8}_${5}`,
+      },
+      {
+        id: 9,
+        notificationID: 5,
+        producerID: 5,
+        name: 'Peruna',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita.',
+        unit: 'g',
+        uniqueId: `${9}_${5}`,
+      },
+      // Marjatila Tynrikka (producerID: 2, ilmoitus id:2)
+      {
+        id: 10,
+        notificationID: 2,
+        producerID: 2,
+        name: 'Vadelma',
+        price: 6,
+        amount: 500,
+        inventory: 100,
+        totalprice: 2000,
+        description:
+          'Täysikypsää, tummaa ja raikkaita marjoja Marjatila Tynrikalta.',
+        unit: 'g',
+        uniqueId: `${10}_${2}`,
+      },
+      {
+        id: 11,
+        notificationID: 2,
+        producerID: 2,
+        name: 'Pensasmustikka',
+        price: 10,
+        amount: 500,
+        inventory: 100,
+        totalprice: 5000,
+        description:
+          'Aitoa suomalaista pensasmustikkaa, täynnä antioksydantteja.',
+        unit: 'g',
+        uniqueId: `${11}_${2}`,
+      },
+      {
+        id: 12,
+        notificationID: 2,
+        producerID: 2,
+        name: 'Peruna',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1500,
+        description: 'Tuoreita perunoita suoraan tilalta.',
+        unit: 'g',
+        uniqueId: `${12}_${2}`,
+      },
+      // Matias tila (producerID: 4, ilmoitus id:4)
+      {
+        id: 13,
+        notificationID: 4,
+        producerID: 4,
+        name: 'Vadelma',
+        price: 6,
+        amount: 500,
+        inventory: 100,
+        totalprice: 2000,
+        description: 'Täysikypsää, tummaa ja raikkaita marjoja Matias tilalta.',
+        unit: 'g',
+        uniqueId: `${13}_${4}`,
+      },
+      {
+        id: 14,
+        notificationID: 4,
+        producerID: 4,
+        name: 'Pensasmustikka',
+        price: 10,
+        amount: 500,
+        inventory: 100,
+        totalprice: 5000,
+        description:
+          'Aitoa suomalaista pensasmustikkaa, täynnä antioksydantteja.',
+        unit: 'g',
+        uniqueId: `${14}_${4}`,
+      },
+      {
+        id: 15,
+        notificationID: 4,
+        producerID: 4,
+        name: 'Porkkana',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita.',
+        unit: 'g',
+        uniqueId: `${15}_${4}`,
+      },
+      // Annan tila (producerID: 6, ilmoitus id:6)
+      {
+        id: 16,
+        notificationID: 6,
+        producerID: 6,
+        name: 'Porkkana',
+        price: 3,
+        amount: 500,
+        inventory: 100,
+        totalprice: 1200,
+        description: 'Makeita ja rapeita porkkanoita Annan tilalta.',
+        unit: 'g',
+        uniqueId: `${16}_${6}`,
+      },
+      {
+        id: 17,
+        notificationID: 6,
+        producerID: 6,
+        name: 'Pensasmustikka',
+        price: 10,
+        amount: 500,
+        inventory: 100,
+        totalprice: 5000,
+        description:
+          'Aitoa suomalaista pensasmustikkaa, täynnä antioksydantteja.',
+        unit: 'g',
+        uniqueId: `${17}_${6}`,
+      },
+      {
+        id: 18,
+        notificationID: 6,
+        producerID: 6,
+        name: 'Vadelma',
+        price: 6,
+        amount: 500,
+        inventory: 100,
+        totalprice: 2000,
+        description: 'Täysikypsää, tummaa ja raikkaita marjoja Annan tilalta.',
+        unit: 'g',
+        uniqueId: `${18}_${6}`,
       },
     ];
 
@@ -110,7 +299,7 @@ export class InMemoryDataService implements InMemoryDbService {
         producers: 'Marjatila Tynrikka',
         producersImge: '/pexels-neslihan-ozdemir-590391077-17117954.jpg',
         date: '22.9.2025 - 28.9.2025',
-        productsID: [2, 3, 4], // → Vadelma, Pensasmustikka, Peruna
+        productsID: [10, 11, 12], // → Vadelma, Pensasmustikka, Peruna
         pickupTimes: [
           'Perjantaina 23.9, klo 12-14',
           'Lauantaina 24.9, klo 10-12',
@@ -132,7 +321,7 @@ export class InMemoryDataService implements InMemoryDbService {
         producers: 'Apilakosken tila',
         producersImge: '/pexels-trinitykubassek-288621.jpg',
         date: '22.9.2025 - 28.9.2025',
-        productsID: [1, 4, 5], // → Mansikka, Peruna, Porkkana
+        productsID: [4, 5, 6], // → Mansikka, Peruna, Porkkana
         pickupTimes: [
           'Lauantaina 24.9, klo 8-10',
           'Sunnuntaina 25.9, klo 12-14',
@@ -154,7 +343,7 @@ export class InMemoryDataService implements InMemoryDbService {
         producers: 'Matias tila',
         producersImge: '/pexels-pixabay-221100.jpg',
         date: '22.9.2025 - 28.9.2025',
-        productsID: [2, 3, 5], // → Vadelma, Pensasmustikka, Porkkana
+        productsID: [13, 14, 15], // → Vadelma, Pensasmustikka, Porkkana
         pickupTimes: [
           'Lauantaina 24.9, klo 8-10',
           'Sunnuntaina 25.9, klo 12-14',
@@ -176,7 +365,7 @@ export class InMemoryDataService implements InMemoryDbService {
         producers: 'Pekan tila',
         producersImge: '/pexels-carolin-wenske-762365559-26646961.jpg',
         date: '22.9.2025 - 28.9.2025',
-        productsID: [1, 5, 4], // → Mansikka, Porkkana, Peruna
+        productsID: [7, 8, 9], // → Mansikka, Porkkana, Peruna
         pickupTimes: [
           'Torstaina 22.9, klo 10-12',
           'Perjantaina 23.9, klo 14-16',
@@ -200,7 +389,7 @@ export class InMemoryDataService implements InMemoryDbService {
         producers: 'Annan tila',
         producersImge: '/pexels-clement-proust-363898785-21906698.jpg',
         date: '22.9.2025 - 28.9.2025',
-        productsID: [5, 3, 2], // → Porkkana, Pensasmustikka, Vadelma
+        productsID: [16, 17, 18], // → Porkkana, Pensasmustikka, Vadelma
         pickupTimes: [
           'Lauantaina 24.9, klo 8-10',
           'Sunnuntaina 25.9, klo 12-14',
