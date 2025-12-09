352489f1b4ec8270eda91d2a7e8111596b372ac4
Maya Ramo <ad5721@student.jamk.fi>
2025-12-09 12:49:49 +0200

    Ilmoitus komponentti on kommentoitu

--- frontend/src/app/ilmoitukset/ilmoitukset.component.ts
+++ frontend/src/app/ilmoitukset/ilmoitukset.component.ts
@@ -1,10 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { AppNotification } from '../types';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NotificationService } from '../notification.service';
-import { IlmoitusTiedot, KaikkiIlmoitusTiedot } from '../types';
+import { IlmoitusTiedot } from '../types';
 
 @Component({
   selector: 'app-ilmoitukset',
@@ -14,20 +12,29 @@ import { IlmoitusTiedot, KaikkiIlmoitusTiedot } from '../types';
   styleUrl: './ilmoitukset.component.css',
 })
 export class IlmoituksetComponent {
+  // muuttuija,joka sisältää kaikki ilmoitukset, jotka backend palauttaa
   notifications: IlmoitusTiedot[] = [];
+
+  // ilmoitukset muuttuija, jotka näkyvät näytöllä. esim. kun syötämme hakukentässä
   filteredNotifications: IlmoitusTiedot[] = [];
+
+  // muuttuija, joka tallennetaan hakukentän arvo kuten jyväskylä
   searchTerm: string = '';
 
+  // Tällä saamme NotificationService-palvelun käyttöön
   constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
+    // ladataan ilmoituksia komponenttissa.
     this.getNotifications();
   }
 
+  // funktio, joka hae notification servicesta API kutsuja getNotifications() funktion kautta ilmoituksia komponenttin
   getNotifications(): void {
     this.notificationService.getNotifications().subscribe({
       next: (data) => {
         this.notifications = data;
+        // kopioi saman listan filteredNotifications-muuttujaan
         this.filteredNotifications = data;
         console.log('Kaikki ilmoitukset:', data);
       },
@@ -35,11 +42,15 @@ export class IlmoituksetComponent {
     });
   }
 
-  // Hakukenttä-funktio
+  // Hakukenttä-funktio, joka haetaan etusivulla ne ilmoitukset (ilmoituksen otsikkoon ja maakunnan perustteella)
   onSearch(event: Event): void {
+    // otetaan käyttäjän kirjoittama hakusana
     const target = event.target as HTMLInputElement;
+
+    // tallennetaan se searchTerm-muuttujaan
     this.searchTerm = target.value.trim().toLowerCase();
 
+    // haemme ilmoituksia (ilmoituksen otsikkon ja maakunnan perusteella)
     this.filteredNotifications = this.notifications.filter(
       (n) =>
         n.title.toLowerCase().includes(this.searchTerm) ||
@@ -47,12 +58,13 @@ export class IlmoituksetComponent {
     );
   }
 
-  // clear-funktion tyhjennuksen varten
+  // Hakukentä tyhjennys ja sama aikaa näyttää kaikki ilmoitukset näkyviin
   clearSearch(): void {
     this.searchTerm = '';
     this.filteredNotifications = this.notifications;
   }
 
+  // käyttää tätä funktioita nopeuttamaan listan löydäminen.
   trackById(index: number, item: IlmoitusTiedot) {
     return item.ilmoitusID;
   }
